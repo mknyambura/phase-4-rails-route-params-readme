7cac9bb434785a783950fb90f06f8ff40f65e7af
mknyambura <kariuki.mercie@gmail.com>
2023-03-14 22:27:23 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #send JSON response using that cheese object
+    cheese = Cheese.find_by(id: params[:id])
+    #find a cheese using the id from the URL
+    render json: cheese
+  end
+
 end
